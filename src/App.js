d59ac661159fb738522eb4ccd2efbcaa0626f81b
Cory <corys4139@gmail.com>
2021-04-02 14:51:44 -0400

    create dropdown with hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dropdown from './components/dropdown';
+
+const data = [
+	{ value: 1, name: "A" },
+	{ value: 2, name: "B" },
+	{ value: 3, name: "C" },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dropdown options={data}/>
     </div>
   );
 }
