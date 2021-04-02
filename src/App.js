cd9e311ea30ce5eb703f93c075ea9cadd89799cb
Cory <corys4139@gmail.com>
2021-04-02 14:58:08 -0400

    create spotify credentials component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Dropdown from './components/dropdown';
+import React, {useState} from "react";
+import { Credentials } from './components/credentials'
+import "./App.css";
+import Dropdown from "./components/dropdown";
+import axios from 'axios'
 
-const data = [
-	{ value: 1, name: "A" },
-	{ value: 2, name: "B" },
-	{ value: 3, name: "C" },
-];
 
 function App() {
+  const data = [
+    { value: 1, name: "A" },
+    { value: 2, name: "B" },
+    { value: 3, name: "C" },
+  ];
+  const spotify = Credentials();
+  const [token, setToken] = useState("")
   return (
-    <div className="App">
-      <Dropdown options={data}/>
-    </div>
-  );
+		<div className="App">
+			<Dropdown options={data} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,15 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Dropdown = (props) => {
-  const [selectedValue, setSelectedValue] = useState('')
-  return ( 
-    <div>
-      <select value={selectedValue} onChange={e => setSelectedValue(e.target.value)}>
-        {props.options.map((item, idx) => <option key={idx} value={item.value}>{item.name}</option>)}
-      </select>
-      <p>{selectedValue}</p>
-    </div>
-   );
-}
- 
+	const [selectedValue, setSelectedValue] = useState("");
+	return (
+		<div>
+			<select
+				value={selectedValue}
+				onChange={(e) => setSelectedValue(e.target.value)}>
+				{props.options.map((item, idx) => (
+					<option key={idx} value={item.value}>
+						{item.name}
+					</option>
+				))}
+			</select>
+			<p>{selectedValue}</p>
+		</div>
+	);
+};
+
 export default Dropdown;
